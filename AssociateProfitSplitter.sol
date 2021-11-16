61887094ba4b483004f2cb0d6b9a1bc4cfd832f2
simongs10 <81064364+simongs10@users.noreply.github.com>
2021-11-16 20:51:14 +1100

    Add files via upload

---
+++ AssociateProfitSplitter.sol
@@ -0,0 +1,31 @@
+contract AssociateProfitSplitter {
+    // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
+    address payable employee_one;
+    address payable employee_two;
+    address payable employee_three;
+    
+    constructor(address payable _one, address payable _two, address payable _three) public {
+        employee_one = _one;
+        employee_two = _two;
+        employee_three = _three;
+    }
+
+    function balance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function deposit() public payable {
+        uint amount = msg.value / 3;
+        employee_one.transfer(amount);
+        employee_two.transfer(amount);
+        employee_three.transfer(amount);
+
+        // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
+        msg.sender.transfer(msg.value - amount * 3);
+    }
+
+    function() external payable {
+        // @TODO: Enforce that the `deposit` function is called in the fallback function!
+        deposit();
+    }
+}
